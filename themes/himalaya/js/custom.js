bd65af3179d981937da82cddc314ea7b9b7a68c7
sumitkumar <manudogra3021@gmail.com>
2016-06-16 17:39:29 +0530

    added css and js

--- themes/himalaya/js/custom.js
+++ themes/himalaya/js/custom.js
@@ -44,37 +44,20 @@
           $('html, body').animate({scrollTop: 0}, duration);
           return false;
         });*/
-        
- 
-        var offset = 250;
-         
-        var duration = 300;
-         
-        jQuery(window).scroll(function() {
-         
-        if (jQuery(this).scrollTop() > offset) {
-         
-        jQuery(‘'.back-to-top').fadeIn(duration);
-         
-        } else {
-         
-        jQuery('.back-to-top’').fadeOut(duration);
-         
-        }
-         
+        $(window).scroll(function(){
+          if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn();
+          } else {
+            $('.back-to-top').fadeOut();
+          }
         });
-         
-                 
-        jQuery('.back-to-top').click(function(event) {
-         
-        event.preventDefault();
-         
-        jQuery('html, body').animate({scrollTop: 0}, duration);
-         
-        return false;
-         
+  
+        //Click event to scroll to top
+        $('.back-to-top').click(function(){
+          $('html, body').animate({scrollTop : 0},800);
+          return false;
         });
-         
+                        
        
         // js for to wrap div in one 
         jQuery("#footer #block-ourwork h2").click(function(){
